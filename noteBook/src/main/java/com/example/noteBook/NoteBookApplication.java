710925c8bd9953d07510cc5a1474e8ad66d00c4e
안경호 <akh1216@gmail.com>
2023-03-18 17:53:55 +0900

    FEAT: 프레임워크 구성

--- noteBook/src/main/java/com/example/noteBook/NoteBookApplication.java
+++ noteBook/src/main/java/com/example/noteBook/NoteBookApplication.java
@@ -1,4 +1,4 @@
-package com.example.noteBook;
+package com.example.NoteBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NoteBookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NoteBookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NoteBookApplication.class, args);
+    }
 
 }
---
+++ noteBook/src/main/java/com/example/noteBook/controller/testController.java
@@ -0,0 +1,13 @@
+package com.example.NoteBook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+    @RequestMapping("/")
+    public String test() {
+        System.out.println("test");
+        return "index";
+    }
+}
