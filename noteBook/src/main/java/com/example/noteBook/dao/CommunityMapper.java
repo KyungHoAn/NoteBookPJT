8f0455975c72178631c30b9eba1c63c6d630a15f
안경호 <akh1216@gmail.com>
2023-04-24 22:40:52 +0900

    FEAT: community map

--- noteBook/src/main/java/com/example/noteBook/dao/CommunityMapper.java
+++ noteBook/src/main/java/com/example/noteBook/dao/CommunityMapper.java
@@ -2,10 +2,16 @@ package com.example.NoteBook.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
 import java.util.Map;
 
 @Mapper
 public interface CommunityMapper {
     void insertCommunity(Map<String, Object> params);
     String getCommunityTag(Map<String, Object> params);
+    List<Map<String, Object>> getCommunityList(Map<String, Object> params);
+    String getCommunityListCnt(int count);
+    Map<String, Object> getCommunityDetail(Map<String, Object> params);
+    void updateCommunity(Map<String, Object> params);
+    void deleteCommunity(Map<String, Object> params);
 }
